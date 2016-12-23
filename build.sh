e5ef03443cdd23f6d5ed020498b09a7e067209c4
Vilson Vieira <vilson@void.cc>
2016-12-22 22:39:45 -0200

    Upload to AWS S3 instead of FTP

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Based on https://gist.github.com/chrismdp/6c6b6c825b07f680e710
+function putS3 {
+  file=$1
+  aws_path=$2
+  aws_id=$3
+  aws_token=$4
+  aws_bucket=$5
+  date=$(date +"%a, %d %b %Y %T %z")
+  acl="x-amz-acl:public-read"
+  content_type='application/x-compressed-tar'
+  string="PUT\n\n$content_type\n$date\n$acl\n/$aws_bucket$aws_path$file"
+  signature=$(echo -en "${string}" | openssl sha1 -hmac "${aws_token}" -binary | base64)
+  curl -X PUT -T "$file" \
+    -H "Host: $aws_bucket.s3.amazonaws.com" \
+    -H "Date: $date" \
+    -H "Content-Type: $content_type" \
+    -H "$acl" \
+    -H "Authorization: AWS ${aws_id}:$signature" \
+    "https://$aws_bucket.s3.amazonaws.com$aws_path$file"
+}
+
 # These should be set from the outside. A git version and heroku/travis respectively
 if [ -z "$VERSION" ]
 then
@@ -20,9 +41,9 @@ make -j4
 cd -
 tar czf libccv-${VERSION}-${TARGET}.tgz -C ccv/lib .
 
-if [ -z "$FTP_PASSWORD" ];
+if [ -z "$AMAZON_API_TOKEN" ];
 then
-    echo "FTP_PASSWORD not provided, skipping upload";
+    echo "Amazon API Token not provided, skipping upload";
 else
-    curl --ftp-create-dirs -T libccv-${VERSION}-${TARGET}.tgz -u ${FTP_USER}:${FTP_PASSWORD} ${FTP_SERVER}/
+    putS3 "libccv-${VERSION}-${TARGET}.tgz" "/bundles/" $AMAZON_API_ID $AMAZON_API_TOKEN $AMAZON_API_BUCKET
 fi
